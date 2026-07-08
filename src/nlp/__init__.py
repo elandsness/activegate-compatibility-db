ab19a99331d5d3324ad67ea331b447692326b5ce
Erik Landsness <erik.landsness@dynatrace.com>
2026-07-08 09:07:31 -0400

    fix: align isort with black and resolve CI formatting

--- src/nlp/__init__.py
+++ src/nlp/__init__.py
@@ -1,5 +1,7 @@
-from src.nlp.compatibility_extractor import (CompatibilityExtractor,
-                                             CompatibilityStatement)
+from src.nlp.compatibility_extractor import (
+    CompatibilityExtractor,
+    CompatibilityStatement,
+)
 from src.nlp.entity_extractor import EntityExtractor, VersionParser
 from src.nlp.nlp_pipeline import ExtractionResult, FactConverter, NLPPipeline
 
